a13b24cfebd523554470ca682c41518b643485a0
Chinzorig Dorjderem <chinzorigdorjderem@Chinzorigs-MacBook-Pro.local>
2023-03-06 18:18:57 +0800

    Add Healthcheck endpoint to auth service

--- grpc-nest-api-gateway/src/health/health.controller.ts
+++ grpc-nest-api-gateway/src/health/health.controller.ts
@@ -1,17 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
+import { GrpcOptions } from '@nestjs/microservices';
 import {
+  GRPCHealthIndicator,
   HealthCheck,
   HealthCheckService,
   HttpHealthIndicator,
   TypeOrmHealthIndicator,
 } from '@nestjs/terminus';
+import { AUTH_SERVICE_NAME, protobufPackage } from 'src/auth/auth.pb';
 
 @Controller('health')
 export class HealthController {
   constructor(
     private health: HealthCheckService,
     private http: HttpHealthIndicator,
-    private db: TypeOrmHealthIndicator,
+    private grpc: GRPCHealthIndicator,
   ) {}
 
   @Get('http')
@@ -19,6 +22,19 @@ export class HealthController {
   checkHttp() {
     return this.health.check([
       () => this.http.pingCheck('gateway', 'http://localhost:9200'),
+    ]);
+  }
+
+  @Get('grpc/auth')
+  @HealthCheck()
+  checkAuthGrpc() {
+    return this.health.check([
+      () =>
+        this.grpc.checkService<GrpcOptions>(
+          AUTH_SERVICE_NAME,
+          protobufPackage,
+          { timeout: 2000 },
+        ),
     ]);
   }
 }
